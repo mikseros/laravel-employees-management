12fec3af93bcc3253c1129d21d8414d58617f2cf
mikseros <mmadynski@gmail.com>
2022-03-25 16:01:21 +0100

    Search country by code or name

--- app/Http/Controllers/Backend/CountryController.php
+++ app/Http/Controllers/Backend/CountryController.php
@@ -5,12 +5,16 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CountryStoreRequest;
 use App\Models\Country;
+use Illuminate\Http\Request;
 
 class CountryController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $countries = Country::all();
+        if($request->has('search')) {
+            $countries = Country::where('country_code', 'like', "%{$request->search}%")->orWhere('name', 'like', "%{$request->search}%")->get();
+        }
         return view('countries.index', compact('countries'));
     }
 
