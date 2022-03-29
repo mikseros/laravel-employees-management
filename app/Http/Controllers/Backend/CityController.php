445d1cc5ea9b0077a175fadfa8b61dfd9a98816b
mikseros <mmadynski@gmail.com>
2022-03-29 10:39:10 +0200

    Delete City

--- app/Http/Controllers/Backend/CityController.php
+++ app/Http/Controllers/Backend/CityController.php
@@ -84,8 +84,10 @@ class CityController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(City $city)
     {
-        //
+        $city->delete();
+
+        return redirect()->route('cities.index')->with('message', 'City Deleted Successfully');
     }
 }
